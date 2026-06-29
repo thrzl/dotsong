8a90e689d7227dcf2e252fb6b5613c6657c58532
thrzl <thrizzle@proton.me>
2026-06-29 00:06:09 -0400

    feat: dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
     import "../app.css";
+    import { ModeWatcher } from "mode-watcher";
     let { children } = $props();
 </script>
 
+<ModeWatcher/>
 {@render children?.()}
 
 <style>
+    :global(html, body) {
+        background: black /* don't flashbang people */ 
+    }
     :global(*) {
         cursor: default;
     }
